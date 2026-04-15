5e1de9eaabbbd123fa504cc6f031b9b7fa8db462
Amnever15 <lifeschool.couveuse@gmail.com>
2026-04-15 12:08:11 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -35,6 +35,14 @@ app = FastAPI(
         "url": "https://www.gnu.org/licenses/agpl-3.0.html",
     },
 )
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ------------------------------------------------------------------------------
 # Routers
